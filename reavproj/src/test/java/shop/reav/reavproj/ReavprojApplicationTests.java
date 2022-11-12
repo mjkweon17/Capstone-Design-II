1404a6784b65a0985bca0557a5bcad7b17cce872
민재 권 <mjkweon17@korea.ac.kr>
2022-11-12 15:58:45 +0900

    Initial Project

---
+++ reavproj/src/test/java/shop/reav/reavproj/ReavprojApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.reav.reavproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReavprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
